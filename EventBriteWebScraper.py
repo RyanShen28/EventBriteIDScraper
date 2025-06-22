fe68096a5497417c2c4ab7e6d5ee5a15967f2b11
Ryan Shen <61341858+RyanShen28@users.noreply.github.com>
2025-06-22 10:47:30 -0700

    Update EventBriteWebScraper.py
    added descriptions

--- EventBriteWebScraper.py
+++ EventBriteWebScraper.py
@@ -10,33 +10,19 @@ import asyncio
 from playwright.async_api import async_playwright, Playwright, TimeoutError, Error
 import json
 import re
-
-
-
-
-#currently Chicago only
-
-
-
-
-
-
-
-
-#todo: fix this
-#
-# future: <Future finished exception=TargetClosedError('Target page, context or browser has been closed')>
-# playwright._impl._errors.TargetClosedError: Target page, context or browser has been closed
-# Future exception was never retrieved
-
-
-#maybe vary sleep time?
-# configuration
-
-
-
+#REMEMBER TO INSTALL ALL PACKAGES
+#currently set to Chicago - modify URL on line 115 as needed
 
 session = requests.Session()
+
+
+emptyFlag = False
+
+# configuration here
+# set max_page to something like 100, the code will stop when it detects no more scrapeable events
+# most cities have something like 50 or so pages of content, Eventbrite claims it
+# has 300+ sometimes but doesn't actually have that many
+
 
 ConcurrentQueries = 5
 
@@ -44,12 +30,12 @@ max_page = 100
 
 sem = asyncio.Semaphore(ConcurrentQueries)
 
-emptyFlag = False
 
 
 async def fetch(browser, url, sem, pageNo, results):
    global emptyFlag
    async with sem:
+
        try:
            url = url+str(pageNo)
 
@@ -102,6 +88,10 @@ async def scrape(url):
            browser = await p.chromium.launch(headless=True)
            try:
                tasks = [fetch(browser, url, sem, pageNo, results) for pageNo in list(range(counter, counter+ConcurrentQueries+1))]
+               #normally I would just have it be for pageNo in range(max_page),
+               #but using locator() somehow causes tasks to break once it finishes
+               #all concurrent queries, and I have no idea why - still trying to debug it
+               #current solution is still somewhat fast but could be MUCH faster
                await asyncio.gather(*tasks, return_exceptions=True)
            finally:
                await browser.close()
@@ -110,9 +100,10 @@ async def scrape(url):
 
 
 
-   with open("eventIDs_chicago.txt", "w") as file:
+   with open("eventIDs_chicago.txt", "a") as file:
        for i in results:
            file.write(i+"\n")
+       file.close()
 
 
 
@@ -126,7 +117,7 @@ async def scrape(url):
 
 
 async def main():
-   url = 'http://eventbrite.com/d/il--chicago/all-events/?page='
+   url = 'http://eventbrite.com/d/il--chicago/all-events/?page=' #replace chicago with city name and il with state abbreviation of choice
    await scrape(url)
 
 
