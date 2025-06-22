85c317115599b0dc371fd77119459f80c4c3abd2
Ryan Shen <61341858+RyanShen28@users.noreply.github.com>
2025-06-22 11:09:04 -0700

    fixed eventbrite api caller

---
+++ EventBriteAPICaller.py
@@ -0,0 +1,27 @@
+import json
+
+import requests
+TOKEN = 'YEMNVG4IIXVPKVNGU2YS'
+
+with open("eventIDS_chicago.txt", "r") as IDFile:
+
+    for line in IDFile:
+        EVENT_ID = line.strip()
+
+        url = f'https://www.eventbriteapi.com/v3/events/{EVENT_ID}/'
+        headers = {
+            'Authorization': f'Bearer {TOKEN}', #my eventbrite private key
+        }
+
+        response = requests.get(url, headers=headers)
+        if response.status_code == 200:
+            eventInfo = response.json()
+            with open('EventInfoChicago.ndjson', 'a') as jsonfile:
+                jsonfile.write(json.dumps(eventInfo)+"\n")
+                jsonfile.close()
+            print("Successful write")
+        else:
+            print(f"Error {response.status_code}: {response.text}")
+
+    IDFile.close()
+
