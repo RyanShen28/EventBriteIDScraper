b5f8832e1e6c3568b5f388d5c72cdf0280e80575
ryanshensmacbook <ryanshenpek@gmail.com>
2025-07-06 01:37:34 -0700

    added comments

--- EventBriteAPICaller.py
+++ EventBriteAPICaller.py
@@ -11,22 +11,78 @@ def scan_socials(s, socials):
 
     if s is None:
         return
+    #Social media patterns listed here:
+    #Pattern for Phone
+
+    phone_pattern = re.compile(
+        r'''
+        (?:               # Non-capturing group for optional parts
+          \d{3}           # 123 (no parentheses)
+          |               # OR
+          \(\d{3}\)      # (123) (with parentheses)
+        )
+        [\s-]?           # Optional space or hyphen
+        \d{3}            # 456 (middle 3 digits)
+        [\s-]?           # Optional space or hyphen
+        \d{4}            # 7890 (last 4 digits)
+        \b               # Word boundary (to avoid longer numbers)
+        ''',
+        re.VERBOSE
+    )
+    email_pattern = re.compile(
+        r"\S+@\S+",
+        re.IGNORECASE,
+    )
+    fb_pattern = re.compile(
+        r'(facebook\.com[^\s]&)',
+        re.IGNORECASE,
+    )
+    insta_link_pattern = re.compile(
+        r'(instagram\.com[^\s]*)',
+        re.IGNORECASE
+    )
+    #pattern1 and pattern2 are listed as such bc easier than having to manually delete the space before the @ for search pattern 1
+    insta_handle_pattern1 = re.compile(
+        r"\s|\A@\S+",
+        re.IGNORECASE
+    )
+    insta_handle_pattern2 = re.compile(
+        r"\A@\S+",
+        re.IGNORECASE
+    )
+
+    phone_matches = phone_pattern.findall(s)
+    if(len(phone_matches) > 0):
+        for i in phone_matches:
+            socials["phone"].append(i)
+
+    #Pattern for Emails
+    email_match = email_pattern.findall(s)
+    if(len(email_match) > 0):
+        for i in email_match:
+            socials["emails"].append(i)
+
+
     # Pattern for Facebook links
-    fb_match = re.findall(r'(facebook\.com[^\s]*)', s, re.IGNORECASE)
+    fb_match = fb_pattern.findall(s)
     if len(fb_match) > 0:
         for i in fb_match:
             socials["fb_link"].append(i)
 
     # Pattern for Instagram link
-    insta_link_match = re.findall(r'(instagram\.com[^\s]*)', s, re.IGNORECASE)
+    insta_link_match = insta_link_pattern.findall(s)
     if len(insta_link_match) > 0:
         for i in insta_link_match:
             socials["insta_link"].append(i)
+
     # Pattern for Instagram handles with @
-    insta_handle_match = re.findall(r'@(\w+)', s)
+    insta_handle_match = insta_handle_pattern1.findall(s)
+    insta_handle_match.extend(insta_handle_pattern2.findall(s))
+
     if len(insta_handle_match) > 0:
         for i in insta_handle_match:
-            socials["insta_handle"].append('@' + i)
+            socials["insta_handle"].append('@'+i.split('@', 1)[1])
+
 
 with open("eventIDS_chicago.txt", "r") as IDFile:
     importantKeys = {"name", "description", "url", "start", "end", "organization", "published", "status", "summary", "facebook_event_id", "organizer_id"}
@@ -53,7 +109,7 @@ with open("eventIDS_chicago.txt", "r") as IDFile:
             #filters json to relevant outputs
 
             eventInfo = {k: v for k, v in eventInfo.items() if k in importantKeys}
-            socials = {"fb_link": [], "insta_link": [], "insta_handle": []}
+            socials = {"fb_link": [], "insta_link": [], "insta_handle": [], "emails":[]}
 
             scan_socials(eventInfo["name"]["text"], socials)
             scan_socials(eventInfo['description']['text'], socials)
